8381e090d20a7e865128ecae325ed6368fe0ad7f
ido graif <ido.graif@gmail.com>
2024-08-09 14:34:24 +0300

    dont use full address, only the street and place name is enough

--- content.js
+++ content.js
@@ -49,7 +49,7 @@ async function fetchReviewData(placeName, placeAddress) {
   const { signal } = reviewModelScoreController;
 
   const apiUrl = `${BASE_SCORE_API_URL}/get_review_data?place_name=${encodeURIComponent(
-    `${placeName} ${placeAddress}`
+    `${placeName} ${placeAddress.split(",")[0]}`
   )}&number_of_reviews=100`;
 
   showSkeletonLoader();
