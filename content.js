fbe6194a6bf1b6a63306b1b20cbfc29447777047
ido graif <ido.graif@gmail.com>
2024-08-31 18:18:34 +0300

    change to server

--- content.js
+++ content.js
@@ -7,8 +7,8 @@ let currentPlaceName = null;
 let currentPlaceAddress = null;
 let history = [];
 const BASE_SCORE_API_URL =
-  // "https://score-google-place-api-bnwzz3dieq-zf.a.run.app";
-  "http://localhost:8000";
+  "https://score-google-place-api-bnwzz3dieq-zf.a.run.app";
+// "http://localhost:8000";
 let isDebugMode = true; // Set this to false to turn off logging
 let reviewModelScoreController = null;
 
